20b62ae98a5ef013093949ef40d0546db960feb0
chris dziedzic <cdziedzic@gmail.com>
2023-08-02 00:50:16 -0400

    added loop to display local storage items

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ let currentTime = dayjs().hour()
 let timeBox = document.querySelectorAll('.description')
 let saveBox = document.querySelectorAll('.saveBtn')
 
-// savedBtn.on("click", function () {
-//   localStorage.setItem("enteredText", $('#savedBtn').parent().children('textArea').val());
-// });
-
 saveBtn.on('click', function (event) {
   for (let i = 0; i < timeBox.length; i++) {
     localStorage.setItem(i, $(saveBox[i]).parent().children('textArea').val())
@@ -17,7 +13,11 @@ saveBtn.on('click', function (event) {
 });
 
 
-
+function populateSaved () {
+  for (let i = 0; i < timeBox.length; i++) {
+    $(saveBox[i]).parent().children('textArea').text(localStorage[i])
+  }
+}
 
 checkTime();
 
@@ -43,20 +43,6 @@ function checkTime() {
     timeBox[i].parentElement.classList.add('past')
 }}}
  
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
- 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
   $("#currentDay").text(dayjs().format('dddd, DD, MMMM'));
+
+  populateSaved();
