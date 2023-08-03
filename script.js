ce7e50615b4795f335de382706c41e862a00027e
chris dziedzic <cdziedzic@gmail.com>
2023-08-03 13:09:16 -0400

    fixed populating text items from localstorage

--- script.js
+++ script.js
@@ -7,57 +7,61 @@ let blockEl = document.querySelectorAll('[data-time]')
 let entries = JSON.parse(localStorage.getItem('inputItems')) || []
 
 saveBtn.on('click', function (event) {
+  // save the block of text for a specific hour to localstorage
   let textEl = $(this).siblings('.description').val()
   let time = $(this).parent().attr('id');
   let dataEl = this.getAttribute('data-time')
-  let entry = {
-    timeEl: time , 
-    enteredText: textEl,
-    dataBox: dataEl
-  }    
+ 
 
-    entries.push(entry)
-    localStorage.setItem("inputItems", JSON.stringify(entries))
+  localStorage.setItem(dataEl, textEl)
+});
+
+function populateSaved() {
+
+  for (let i = 0; i < timeBox.length; i++) {
+ 
+   
+    // if the local storage getitem for the current dataset.time is null we do nothing
+    if (localStorage.getItem(timeBox[i].dataset.time) === null){
+      timeBox[i].value = ''
+      
+    } else {
+     
+      timeBox[i].value = localStorage.getItem(timeBox[i].dataset.time)
+    }
+
+   
+
+  }
+};
 
 
-    
-    
-    
-    
-    });
-    
-    //  populateSaved() {
-
-    // document.querySelector("textArea").hasAttribute()
-
-    // loop over the textarea 
-    //     loop over every object in 
-
-
-    checkTime();
+checkTime();
 
 function checkTime() {
-  
+  // Check if data-time value against dayjs hour value, change color red=current hour, grey= past, green = future
   for (let i = 0; i < timeBox.length; i++) {
-    
-  if (timeBox[i].dataset.time == currentTime) {
-    timeBox[i].parentElement.classList.remove('future')
-    timeBox[i].parentElement.classList.remove('past')
-    timeBox[i].parentElement.classList.add("present")
+
+    if (timeBox[i].dataset.time == currentTime) {
+      timeBox[i].parentElement.classList.remove('future')
+      timeBox[i].parentElement.classList.remove('past')
+      timeBox[i].parentElement.classList.add("present")
+    }
+
+    if (timeBox[i].dataset.time > currentTime) {
+      timeBox[i].parentElement.classList.remove("present")
+      timeBox[i].parentElement.classList.remove('past')
+      timeBox[i].parentElement.classList.add('future')
+
+    }
+    if (timeBox[i].dataset.time < currentTime) {
+      timeBox[i].parentElement.classList.remove("present")
+      timeBox[i].parentElement.classList.remove('future')
+      timeBox[i].parentElement.classList.add('past')
+    }
   }
+}
+//display at top of page
+$("#currentDay").text(dayjs().format('dddd, DD, MMMM'));
 
-  if (timeBox[i].dataset.time > currentTime) {
-    timeBox[i].parentElement.classList.remove("present")
-    timeBox[i].parentElement.classList.remove('past')
-    timeBox[i].parentElement.classList.add('future')
-
-  }
-  if (timeBox[i].dataset.time < currentTime) {
-    timeBox[i].parentElement.classList.remove("present")
-    timeBox[i].parentElement.classList.remove('future')
-    timeBox[i].parentElement.classList.add('past')
-}}}
- 
-  $("#currentDay").text(dayjs().format('dddd, DD, MMMM'));
-
-  // populateSaved();
+populateSaved();
